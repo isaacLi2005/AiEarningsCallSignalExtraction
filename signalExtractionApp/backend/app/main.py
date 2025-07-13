596f64b94867a6571ecac9fe3d0e983e7f61a0a6
Isaac Li <isaacli@andrew.cmu.edu>
2025-07-13 12:32:48 -0400

    Able to pull a transcript from API Ninjas

--- signalExtractionApp/backend/app/main.py
+++ signalExtractionApp/backend/app/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+import requests
 
 app = FastAPI()
+
+load_dotenv()
+API_NINJAS_API_KEY = os.getenv("API_NINJAS_KEY")
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,5 +19,20 @@ app.add_middleware(
 )
 
 @app.get("/ping")
-def pint():
-    return {"message": "pong"}
+def ping():
+    return {"message": f"pong, API key is {API_NINJAS_API_KEY}"}
+
+@app.get("/transcript")
+def transcript(
+    ticker: str = Query("NVDA"),
+    year: int = Query(...),
+    quarter: int = Query(...)
+):
+    headers = {"X-Api-Key": API_NINJAS_API_KEY}
+    url = f"https://api.api-ninjas.com/v1/earningstranscript?ticker={ticker}&year={year}&quarter={quarter}"
+
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return {"error": "Failed to fetch transcript", "status_code": response.status_code, "details": response.text}
+    
+    return response.json()
--- signalExtractionApp/frontend/src/App.tsx
+++ signalExtractionApp/frontend/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface PingResponse {
+  message: string;
 }
 
-export default App
+function App() {
+  const [msg, setMsg] = useState<string> ("loading...");
+
+  useEffect(
+    () => {
+      fetch("http://localhost:8000/ping")
+        .then((res) => res.json())
+        .then((data: PingResponse)=> {
+          setMsg(data.message);
+        })
+        .catch((error) => {
+          console.error("Error contacting backend: ", error);
+          setMsg("Backend unreachable");
+        })
+    }
+  , []);
+}
