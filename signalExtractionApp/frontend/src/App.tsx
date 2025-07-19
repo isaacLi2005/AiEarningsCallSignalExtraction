b6749810a253a2afb5d34bb41e742ec9ee92af7f
Isaac Li <isaacli@andrew.cmu.edu>
2025-07-19 15:46:46 -0400

    Updated requirements.txt

--- signalExtractionApp/frontend/src/App.tsx
+++ signalExtractionApp/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import SentimentAndKeywordsLayoutBuilder from "./components/mainLayout";
 import SentimentChart from "./components/sentimentChart";
