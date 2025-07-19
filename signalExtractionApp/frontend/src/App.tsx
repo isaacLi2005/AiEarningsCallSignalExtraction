c558dd5ec1574a700a8112023d86f8cd8a9fcbf5
Isaac Li <isaacli@andrew.cmu.edu>
2025-07-18 20:28:35 -0400

    Ticker select form

--- signalExtractionApp/frontend/src/App.tsx
+++ signalExtractionApp/frontend/src/App.tsx
@@ -1,40 +1,45 @@
 import {useEffect, useState} from "react";
 
+import TickerSelectForm from "./components/tickerSelect";
+
 const backendURL = import.meta.env.VITE_BACKEND_URL
 
+
 function App() {
-  const [msg, setMsg] = useState<string> ("loading...");
+  const [result, setResult] = useState<string> ("Waiting for ticker selection...");
 
-  useEffect(
-    () => {
-      fetch(`${backendURL}/analyze_last_n_quarters_sentiment?ticker=NVDA&n=4`)
-        .then((res) => res.json())
-        .then((data)=> {
-          setMsg(data.results);
-        })
-        .catch((error) => {
-          console.error("Error contacting backend: ", error);
-          setMsg("Backend unreachable");
-        })
+    function fetchTickerData(ticker: string) {
+      setResult("Loading...");
+      fetch(`${backendURL}/analyze_last_n_quarters_sentiment?ticker=${ticker}&n=4`)
+              .then((res) => res.json())
+              .then((data)=> {
+                setResult(data.results);
+              })
+              .catch((error) => {
+                console.error("Error contacting backend: ", error);
+                setResult("Backend unreachable or error. ");
+              });
     }
-  , []);
+
+    function handleTickerSubmit(ticker: string) {
+      fetchTickerData(ticker);
+    }
 
 
   return (
-    <div>
-      <h1> PUSH THE BUTTON </h1>
-      <MyButton />
+    <main style={{ padding: "1rem", fontFamily: "sans-serif" }}>
+      <h1>AI Earnings Signal</h1>
 
-      <h2> {JSON.stringify(msg, null, 2)} </h2>
-    </div>
-  )
-}
+      <TickerSelectForm onSubmit={handleTickerSubmit} />
 
-
-function MyButton() {
-  return (
-    <button onClick={() => console.log("Clicked")}> I'm a button </button>
-  )
+      <h2>Backend Response</h2>
+      <pre>
+        {typeof result === "string"
+          ? result
+          : JSON.stringify(result, null, 2)}
+      </pre>
+    </main>
+  );
 }
 
 
